820e448434da5cf59e3c9d5098537bea16f0c43e
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-14 00:51:12 +0200

    added øamdomg component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
+import { Parallax } from "react-materialize";
+
 class Landing extends Component {
   render() {
     return (
         <div>
-            <h1>HELLO WORLD</h1>
+            <Parallax imageSrc="/img/landing.jpg"/>
+            <div className="section white">
+                <div className="row container">
+                <h2 className="header">Parallax</h2>
+                <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                </div>
+            </div>
         </div>
     )
   }
