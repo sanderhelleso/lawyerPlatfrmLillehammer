e9357bf57cd45faaf72041f5a58e9ff1e08abf0f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-14 16:51:44 +0200

    added dropdown

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Col, Card, CardTitle, Tabs, Tab, Collection, CollectionItem, Icon } from "react-materialize";
+import { Col, Card, CardTitle, Tabs, Tab, Collection, CollectionItem, Icon, Input, Row } from "react-materialize";
 
 class Articles extends Component {
     renderRecent() {
@@ -26,12 +26,12 @@ class Articles extends Component {
     renderArchive() {
         const months = ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "oktober", "september", "november", "desember"];
         return months.map((month) =>
-            <Col key={month} m={4} s={12} className="animated fadeIn">
-                <Card className="cardHeader z-depth-2" header={<a href={`/arkiv/${month}`}> <CardTitle id="archiveCard" reveal image={`/img/img${months.indexOf(month) + 1}.jpg`} waves='light'>{month.toUpperCase()} </CardTitle> </a>}
-                    title={month.toUpperCase()}
+            <Col key={month} l={4} m={6} s={12} className="animated fadeIn">
+                <Card className="cardHeader z-depth-2" header={<a href={`/arkiv/${new Date().getFullYear()}/${month}`}><CardTitle id="archiveCard" reveal image={`/img/img${months.indexOf(month) + 1}.jpg`} waves='light'>{month.toUpperCase()} </CardTitle> </a>}
+                    title={`JUSSPOST #${months.indexOf(month) + 1}`}
                     reveal={
                         <div>
-                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            <p className="cardIntro">Here is some more information about this product that is only revealed once clicked on.</p>
                             <Collection>
                                 <CollectionItem href={`/arkiv/${month}/manedenssak`}><Icon small left>account_balance</Icon> Månedens sak</CollectionItem>
                                 <CollectionItem href={`/arkiv/${month}/femkjappe`}><Icon small left>assignment</Icon> 5 kjappe</CollectionItem>
@@ -39,7 +39,8 @@ class Articles extends Component {
                             </Collection>
                         </div>
                     }>
-                    <p><a href={`/arkiv/${month}`}>Les mer</a></p>
+                    <p className="cardIntro">Here is some more information about this product that is only revealed once clicked on.</p>
+                    <p><a className="teal-text font-weight-bold" href={`/arkiv/${new Date().getFullYear()}/${month}`}>Les mer</a></p>
                 </Card>
             </Col>
         );
@@ -55,9 +56,17 @@ class Articles extends Component {
                     </Tab>
                     <Tab title="Arkiv" className="animated fadeIn" active>
                         <h3 id="recentTxt" className="center-align animated fadeInLeft">Arkiv</h3>
-                        <div className="row">
+                        <Row>
+                            <Col l={10} m={9} s={2}/>
+                            <Input className="animated fadeIn" l={2} m={3} s={8} type='select' label="År" defaultValue='1'>
+                                <option value='1'>2018</option>
+                                <option value='2'>2019</option>
+                                <option value='3'>2020</option>
+                            </Input>
+                        </Row>
+                        <Row>
                             {this.renderArchive()}
-                        </div>
+                        </Row>
                     </Tab>
                 </Tabs>
             </div>
