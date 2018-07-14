7a86dbbb83bf32c3632c11b7c0d3767283d4e7de
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-14 02:16:28 +0200

    added cards

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
-
-import { Parallax } from "react-materialize";
+import { Parallax, Col, Card, CardTitle } from "react-materialize";
 
 class Landing extends Component {
-  render() {
-    return (
-        <div>
-            <div className="genreOverlay">
-                <Parallax id="landingCover" imageSrc="/img/landing.jpg"/>
-            </div>
-            <div className="section white">
-                <div className="row container">
-                <h2 className="header">Parallax</h2>
-                <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+
+    renderContent() {
+        const months = ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "oktober", "september", "november", "desember"];
+        return months.map((month) =>
+            <Col key={month} m={4} s={12}>
+                <Card className="cardHeader" header={<CardTitle reveal image={`/img/img${months.indexOf(month) + 1}.jpg`} waves='light'>{month.toUpperCase()} </CardTitle>}
+                    title={month.toUpperCase()}
+                    reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+                    <p><a href="#">Les mer</a></p>
+                </Card>
+            </Col>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 id="heading">JUSSBLOGG</h1>
+                <div className="genreOverlay">
+                    <Parallax id="landingCover" imageSrc="/img/landing.jpg"/>
+                </div>
+                <div className="section white">
+                    <h3 id="recentTxt" className="center-align">Nylige innlegg</h3>
+                    <div className="row container">
+                        {this.renderContent()}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
 
 export default Landing;
