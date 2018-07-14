6def88aa73abe3d25485fb9e2a0bde7ed384ce8e
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-14 23:54:36 +0200

    added api route for recent

--- routes/recentRoutes.js
+++ routes/recentRoutes.js
@@ -1,6 +1,13 @@
+const mongoose = require("mongoose");
+const Article = mongoose.model("articles");
+const Question = mongoose.model("questions");
+const Tip = mongoose.model("tips");
+
 module.exports = app => {
-    app.get("/sistenytt", (req, res) => {
-        console.log(123);
-        res.send("qwekqlkejqlek");
+    app.get("/api/sistenytt", (req, res) => {
+        
+        Article.find().sort({ _id: -1 }).limit(1).exec((err, data) => {
+            res.send(data);
+        });
     });
 }
