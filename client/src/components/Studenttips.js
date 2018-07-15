b7ddb61893c4b8e175b63bac28a88432b78879ef
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-15 19:47:12 +0200

    added social share buttons

--- client/src/components/Studenttips.js
+++ client/src/components/Studenttips.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Parallax } from "react-materialize";
+import { Parallax, Row, Col } from "react-materialize";
 import { MONTHS } from "../globals/months";
+import { SocialIcon } from 'react-social-icons';
 
 class Studenttips extends Component {
     state = {
@@ -36,6 +37,25 @@ class Studenttips extends Component {
                         <h3 id="layoutTitle">Månedens studenttips</h3>
                         <hr />
                         <p id="layoutBody">{data.body}</p>
+                        <hr id="shareHr" />
+                        <h5 id="shareHeading" className="center-align">Kjenner du noen som kan ha nytte av månedens tips?</h5>
+                        <p id="shareSubTxt" className="center-align">Del på sosiale medier</p>
+                        <div className="container">
+                            <Row id="share">
+                                <Col s={4} m={2} l={2} offset="l2 m2 s2" className="valign-wrapper">
+                                    <SocialIcon className="col s12" network="facebook" url="http://www.facebook.com/sharer.php?u=https://offentligetilfluktsrom.no/" />
+                                </Col>
+                                <Col s={4} m={2} l={2} className="valign-wrapper">
+                                    <SocialIcon network="linkedin" url="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://offentligetilfluktsrom.no/" />
+                                </Col>
+                                <Col s={4} m={2} l={2} offset="s2" className="valign-wrapper">
+                                    <SocialIcon network="twitter" url="https://twitter.com/share?url=https://offentligetilfluktsrom.no/" />
+                                </Col>
+                                <Col s={4} m={2} l={2} className="valign-wrapper">
+                                    <SocialIcon network="google" url="https://plus.google.com/share?url=https://offentligetilfluktsrom.no/" />
+                                </Col>
+                            </Row>
+                        </div>
                     </div>
                 </section>
             )
