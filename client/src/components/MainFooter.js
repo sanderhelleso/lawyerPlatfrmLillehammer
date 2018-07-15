31a6430cb69c46279a42d6441cd4e244b996034f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-15 21:05:44 +0200

    added footer and styles

--- client/src/components/MainFooter.js
+++ client/src/components/MainFooter.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import { Footer } from "react-materialize";
+import 'font-awesome/css/font-awesome.min.css';
+import { Footer, Icon } from "react-materialize";
 
 class MainFooter extends Component {
     render() {
         return (
-            <Footer copyrights="&copy 2015 Copyright Text"
+            <Footer copyrights="&copy; 2018 Jussbloggen"
                 moreLinks={
-                    <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                    <a className="right" href="#!">Utviklet og levert av Semanta</a>
                 }
                 links={
-                    <ul>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    <ul id="footerLinks">
+                        <li><a className="grey-text text-lighten-3" href="#!"><i className="fa fa-facebook-square facebook"></i>
+                        </a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!"><i className="fa fa-linkedin linkedin"></i>
+
+                        </a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!"><i className="fa fa-instagram instagram"></i>
+
+                        </a></li>
                     </ul>
                 }
-                className='example'
                 >
-                    <h5 className="white-text">Footer Content</h5>
-                    <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                    <h5 id="footerHeading">JUSSBLOGGEN</h5>
+                    <hr id="footerHr" />
+                    <p id="footerIntro">Jussbloggen er et frivillig prosjekt utført av jussstudenter i Lillehammer og har som mål å dekke viktige saker samt hjelpe studener</p>
             </Footer>
         )
     }
