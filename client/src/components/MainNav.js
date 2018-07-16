7a0c17bdcc5661f1191b5063c49844adfab6161d
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-16 21:21:11 +0200

    navbar now displays when scrolled past main container

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 import { Navbar, NavItem, Icon } from "react-materialize";
 
 class MainNav extends Component {
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll(event) {
+        const target = document.querySelector(".parallax-container");
+        const nav = document.querySelector(".navbar-fixed");
+        if (window.scrollY > (target.offsetTop + target.offsetHeight)) {
+            nav.style.display = "block";
+        }
+
+        else {
+            nav.style.display = "none";
+        }
+    }
+
     render() {
         return (
-            <Navbar brand='logo' right>
+            <Navbar id="nav" brand='logo' right fixed className="animated fadeInDown" >
                 <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
                 <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
                 <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
@@ -12,6 +33,10 @@ class MainNav extends Component {
             </Navbar>
         )
     }
+}
+
+function displayNav() {
+    console.log(123);
 }
 
 export default MainNav;
