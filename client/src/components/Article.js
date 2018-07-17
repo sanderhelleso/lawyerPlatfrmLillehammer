17b0765f729da234a557b87f8b497dca4ca309ad
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-18 00:23:37 +0200

    can now sort after selected archive year

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -4,16 +4,14 @@ import { MONTHS } from "../globals/months";
 
 class Article extends Component {
     state = {
-        data: [],
-        year: ""
+        data: []
     };
 
     async loadData() {
         const res = await fetch("/api/arkiv");
         const json = await res.json();
         this.setState({
-            data: json,
-            year: document.querySelector("#archiveSelect").value
+            data: json
         })
     }
 
@@ -25,29 +23,25 @@ class Article extends Component {
         const cardData = this.state.data.map((data) => {
             const year = document.querySelector("#archiveSelect").value;
             const monthName = MONTHS[data.month].toUpperCase();
-            console.log(year);
-
-            if (data.year == year) {
-                return (
-                    <Col key={data.article_id} l={4} m={6} s={12} className={`animated fadeIn ${data.year}`}>
-                        <Card className="cardHeader z-depth-2" header={<a href={`/arkiv/${year}/${monthName.toLowerCase()}`}><CardTitle id="archiveCard" reveal image={`/img/img${data.month + 1}.jpg`} waves='light'>{monthName} </CardTitle> </a>}
-                            title={`JUSSPOST #${data.article_id}`}
-                            reveal={
-                                <div>
-                                    <p className="cardIntro">{data.intro}</p>
-                                    <Collection>
-                                        <CollectionItem href={`/arkiv/${year}/${monthName.toLowerCase()}/manedenssak`}><Icon small left>account_balance</Icon> Månedens sak</CollectionItem>
-                                        <CollectionItem href={`/arkiv/${year}/${monthName.toLowerCase()}/femkjappe`}><Icon small left>assignment</Icon> 5 kjappe</CollectionItem>
-                                        <CollectionItem href={`/arkiv/${year}/${monthName.toLowerCase()}/studenttips`}><Icon small left>school</Icon> Studenttips</CollectionItem>
-                                    </Collection>
-                                </div>
-                            }>
-                            <p className="cardIntro">{data.intro}</p>
-                            <p><a className="teal-text font-weight-bold" href={`/arkiv/${year}/${monthName.toLowerCase()}`}>Les mer</a></p>
-                        </Card>
-                    </Col>
-                )
-            }
+            return (
+                <Col key={data.article_id} l={4} m={6} s={12} className={`animated fadeIn archiveCard ${data.year}`}>
+                    <Card className="cardHeader z-depth-2" header={<a href={`/arkiv/${year}/${monthName.toLowerCase()}`}><CardTitle id="archiveCard" reveal image={`/img/img${data.month + 1}.jpg`} waves='light'>{monthName} </CardTitle> </a>}
+                        title={`JUSSPOST #${data.article_id}`}
+                        reveal={
+                            <div>
+                                <p className="cardIntro">{data.intro}</p>
+                                <Collection>
+                                    <CollectionItem href={`/arkiv/${year}/${monthName.toLowerCase()}/manedenssak`}><Icon small left>account_balance</Icon> Månedens sak</CollectionItem>
+                                    <CollectionItem href={`/arkiv/${year}/${monthName.toLowerCase()}/femkjappe`}><Icon small left>assignment</Icon> 5 kjappe</CollectionItem>
+                                    <CollectionItem href={`/arkiv/${year}/${monthName.toLowerCase()}/studenttips`}><Icon small left>school</Icon> Studenttips</CollectionItem>
+                                </Collection>
+                            </div>
+                        }>
+                        <p className="cardIntro">{data.intro}</p>
+                        <p><a className="teal-text font-weight-bold" href={`/arkiv/${year}/${monthName.toLowerCase()}`}>Les mer</a></p>
+                    </Card>
+                </Col>
+            )
         });
         return cardData;
     }
--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -4,6 +4,12 @@ import RecentArticle from './RecentArticle';
 import Article from "./Article";
 
 class Articles extends Component {
+    componentWillMount() {
+        setTimeout(() => {
+            sort();
+        }, 1000);
+    }
+
     render() {
         return (
             <div className="section container">
@@ -16,7 +22,7 @@ class Articles extends Component {
                         <h3 id="recentTxt" className="center-align animated fadeInLeft">Arkivet</h3>
                         <Row>
                             <Col l={10} m={9} s={2}/>
-                            <Input id="archiveSelect" l={2} m={3} s={8} type='select' label="År" defaultValue={new Date().getFullYear()} onChange={sort()}>
+                            <Input id="archiveSelect" l={2} m={3} s={8} type='select' label="År" defaultValue={new Date().getFullYear()} onChange={sort}>
                                 <option value='2018'>2018</option>
                                 <option value='2019'>2019</option>
                                 <option value='2020'>2020</option>
@@ -33,7 +39,17 @@ class Articles extends Component {
 }
 
 function sort() {
-    console.log(123);
+    const selectedYear = document.querySelector("#archiveSelect").value;
+    const articles = document.querySelectorAll(".archiveCard");
+    articles.forEach((article) => {
+        if (article.classList.contains(selectedYear)) {
+            article.style.display = "block";
+        }
+
+        else {
+            article.style.display = "none";
+        }
+    });
 }
 
 export default Articles;
