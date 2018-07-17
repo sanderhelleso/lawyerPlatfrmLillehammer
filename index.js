34835950714c6df58103fedbdb2cea733086505d
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-18 00:53:59 +0200

    edited bug

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ if (process.env.NODE_ENV === "production") {
     const path = require("path");
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+    });
+}
 
 // start server
 server.listen(port, host);
