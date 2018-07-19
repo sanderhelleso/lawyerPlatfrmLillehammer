4875c63eee4b128e5bc22c742c11b90df8e85793
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-19 16:24:35 +0200

    can now find user

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,8 +1,31 @@
-
+const mongoose = require("mongoose");
+require("../models/User");
+const User = mongoose.model("user");
+const bcrypt = require("bcrypt");
 
 module.exports = app => {
     app.post("/api/login", (req, res) => {
-        console.log(req.body);
-        res.send("qweweq");
+        const email = req.body.email;
+        const password = req.body.pass;
+        User.findOne({ email: email })
+            .exec(function (err, user) {
+                console.log(user);
+                if (err) {
+                    return err;
+                }
+
+                else if (!user) {
+                    const err = new Error('User not found.');
+                    err.status = 401;
+                    return err;
+                }
+
+                bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    console.log("LOGGED IN");
+                    return null, user;
+                } 
+            })
+        });
     });
 }
