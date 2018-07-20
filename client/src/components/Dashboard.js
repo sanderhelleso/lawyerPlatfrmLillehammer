66db07cccef2aadc3ab06ebda5ab0e352fd40930
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-20 19:23:55 +0200

    now renders months

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Col, Tabs, Tab, Input, Row, Button } from "react-materialize";
 import TinyMCE from 'react-tinymce';
+import { MONTHS } from "../globals/months";
 
 class Dashboard extends Component {
 
     handleEditorChange(e) {
         console.log(e.target.getContent());
+    }
+
+    renderMonths() {
+        return MONTHS.map((month) =>
+            <option value={MONTHS.indexOf(month)}>{month.toUpperCase()}</option>
+        );
     }
 
     render() {
@@ -16,6 +23,12 @@ class Dashboard extends Component {
                     <p className="center-align text-center">Publiser et nytt innlegg</p>
                     <Row>
                         <Col l={2} m={3} s={1}/>
+                        <Input id="monthSelect" l={8} m={6} s={10} type='select' label="Måned" defaultValue={new Date().getMonth()} >
+                            {this.renderMonths()}
+                        </Input>
+                    </Row>
+                    <Row>
+                        <Col l={2} m={3} s={1}/>
                         <Input id="archiveSelect" l={8} m={6} s={10} type='select' label="Kategori" defaultValue={"1"} >
                             <option value='1'>Månedens Sak</option>
                             <option value='2'>Fem Kjappe</option>
@@ -24,12 +37,12 @@ class Dashboard extends Component {
                     </Row>
                     <Row>
                         <Col l={2} m={3} s={1}/>
-                        <Input l={8} m={6} s={10} label="Introduksjon" type='textarea' />
+                        <Input id="introInput" l={8} m={6} s={10} label="Introduksjon" type='textarea' />
                     </Row>
                     <Row>
-                        <Col l={1} m={3} s={1}/>
-                        <Col l={10}>
-                            <TinyMCE
+                        <Col l={1} m={2} s={1}/>
+                        <Col l={10} m={8} s={10}>
+                            <TinyMCE id="postBody"
                                 content="<p>Once upon a time...</p>"
                                 config={{
                                 plugins: 'autolink link image lists print preview',
@@ -40,9 +53,9 @@ class Dashboard extends Component {
                         </Col>
                     </Row>
                     <Row>
-                        <Col l={4} />
-                        <Col l={4}>
-                            <Button id="publishPost" waves='light' className="col s12 m12 l12">Publiser</Button>
+                        <Col l={4} m={2} s={1}/>
+                        <Col l={4} m={8} s={10}>
+                            <Button id="publishPost" waves='light' className="col s12 m12 l12" onClick={publishPost}>Publiser</Button>
                         </Col>
                     </Row>
                 </div>
@@ -59,6 +72,31 @@ function checkSessionStorage() {
     else {
         return true;
     }
+}
+
+function publishPost() {
+    console.log(123);
+    const postType = document.querySelector("#archiveSelect").value;
+    const postIntro = document.querySelector("#introInput").value;
+    const postBody = document.querySelector("#postBody").value;
+
+    /*fetch(`/api/publishPost`, {
+        method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            category: postType,
+            intro: postIntro,
+            postBody: postBody
+
+        })
+    })
+    .then(res => res.json())
+    .then(res => */
+
+    console.log(postType, postIntro, postBody);
 }
 
 export default Dashboard;
