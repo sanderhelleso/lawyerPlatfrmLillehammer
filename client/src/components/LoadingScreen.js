d018fa485eb8fa9e39d3845bfc39833be6f3c823
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-23 00:43:26 +0200

    added error loading msg

--- client/src/components/LoadingScreen.js
+++ client/src/components/LoadingScreen.js
@@ -8,6 +8,7 @@ class LoadingScreen extends Component {
                     <h1 id="heading" className="center-align">Obiterdictum</h1>
                     <p className="center-align">Jurdiske nyheter og tips for jussstudentene i Lillehammer</p>
                     <p className="loading center-align"></p>
+                    <p id="refresh" className="animated fadeIn"><span>Hmmm, det ser ut som vi har problemer med å laste inn siden</span><br /><br /><a href={`${window.location.href}`}> Prøv på nytt?</a></p>
                 </div> 
             </div>
         )
