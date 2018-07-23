29733f653b6589bf4a3e8667889d91854f7f1907
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-23 19:39:12 +0200

    .

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 require("../models/User");
 const User = mongoose.model("user");
 const bcrypt = require("bcrypt");
@@ -62,4 +62,4 @@ module.exports = app => {
             res.send(403);
         }
     }
-}
+}*/
